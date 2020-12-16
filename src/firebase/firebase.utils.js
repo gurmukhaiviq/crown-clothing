4927084ac190803ece8cea7ff496e0fd4b061240
gurmukhaiviq <75238052+gurmukhaiviq@users.noreply.github.com>
2020-12-16 16:51:40 +0530

    creating sign up

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,7 +12,36 @@ const config = {
     appId: "1:627923636508:web:7411d17b21416e91132ad9",
     measurementId: "G-NW4S3470KZ"
       
-}
+};
+
+export const createUserProfileDocument = async ( userAuth,additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+
+    if(!snapShot.exists) {
+        
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+
+            await userRef.set ({
+displayName,
+email,
+createdAt,
+...additionalData
+            })
+        }catch(error) {
+            console.log('error creating user',error.message);
+        }
+
+    }
+    return userRef;
+};
+
 firebase.initializeApp(config);
  export const auth = firebase.auth();
  export const firestore = firebase.firestore(); 
---
+++ src/test.js
@@ -0,0 +1,6 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+const  firestore = firebase.firestore();
+firebase.collection('users').doc('2BXQuVHY2K5ZJjPswAXD').collection('cartItems').doc('sbDWfpveJM2YyqJhInTA');
+firebase.doc('users/2BXQuVHY2K5ZJjPswAXD/cartItems/sbDWfpveJM2YyqJhInTA');
+firebase.collection('users/2BXQuVHY2K5ZJjPswAXD/cartItems');
