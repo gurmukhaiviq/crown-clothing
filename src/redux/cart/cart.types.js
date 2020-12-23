78457171ff0e39a55b0ffb696a12f834b88c4425
gurmukhaiviq <75238052+gurmukhaiviq@users.noreply.github.com>
2020-12-23 11:07:15 +0530

    created removing and adding cartItems buttons

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,8 @@
 const CartActionTypes = {
     TOGGLE_CASE_HIDDEN: 'TOGGLE_CASE_HIDDEN',
-    ADD_ITEM: 'ADD_ITEM'
+    ADD_ITEM: 'ADD_ITEM',
+    CLEAR_ITEM_FROM_CART : 'CLEAR_ITEM_FROM_CART',
+    REMOVE_ITEM : 'REMOVE_ITEM'
     
 }
 export default CartActionTypes;
