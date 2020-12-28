51de892f58f7ede8bdb566d884dead45fdc88671
gurmukhaiviq <75238052+gurmukhaiviq@users.noreply.github.com>
2020-12-28 14:12:58 +0530

    moved our directoory and shop data into its own reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,28 +1,38 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-// import CollectionPreview from '../collection-preview/collection-preview';
 import CustomButton from '../custom-button/custom-button.component';
-import './collection-item.styles.scss';
 import { addItem } from '../../redux/cart/cart.actions';
 
-const CollectionItem = ({item,addItem }) => {
-    const { name,price,imageUrl } = item;
-    return(
-    <div className='collection-item'>
-        <div className='image' 
-        style = {{backgroundImage: `url(${imageUrl})`}}
-        />
-        <div className='collection-footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
+import './collection-item.styles.scss';
 
-        </div>
-        <CustomButton onClick={ () => addItem(item) } inverted>  Add To Cart </CustomButton>
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+
+  return (
+    <div className='collection-item'>
+      <div
+        className='image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className='collection-footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
     </div>
-)};
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
-    addItem : item => dispatch(addItem(item))
-})
-export default connect ( null, mapDispatchToProps ) (CollectionItem);
+  addItem: item => dispatch(addItem(item))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CollectionItem);
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -8,8 +8,8 @@ const CollectionPreview = ({title,items}) => (
         </h1>
         <div className='preview'>
             {
-              items.filter((item,idx) => idx < 4).map((item) => (
-                  <CollectionItem key={item.id} item = {item}  />
+              items.filter((item,idx) => idx < 4).map(item => (
+                  <CollectionItem key={item.id} item = {item} />
               ))
             }
         </div>
